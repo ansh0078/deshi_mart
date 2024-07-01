9572fa5797bf276eb48d1b93d6acb48894ad8756
Ansh Raj <anshraj0078@gmail.com>
2024-07-01 22:47:05 +0530

    added icons

--- lib/configs/AssetsPath.dart
+++ lib/configs/AssetsPath.dart
@@ -6,10 +6,12 @@ class IconsAssets {
   static String bag = "${basePath}appIcon.svg"; //------
   static String car = "${basePath}car.svg";
   static String cart = "${basePath}cart.svg";
+  static String coupon = "${basePath}coupon.svg";
   static String category = "${basePath}category.svg";
   static String dashBoard = "${basePath}dashBoard.svg";
   static String done = "${basePath}done.svg";
   static String print = "${basePath}print.svg";
+  static String order = "${basePath}order.svg";
   static String refresh = "${basePath}refersh.svg";
   static String setting = "${basePath}settings.svg";
   static String products = "${basePath}products.svg";
--- lib/pages/web/HomePage/HomePage.dart
+++ lib/pages/web/HomePage/HomePage.dart
@@ -34,20 +34,44 @@ class WebHomePage extends StatelessWidget {
                     isSelected: drawerProvider.selectedPageIndex == 0,
                   ),
                   MyDrawerMenu(
-                    title: "Category",
-                    icon: IconsAssets.category,
+                    title: "Products",
+                    icon: IconsAssets.products,
                     onPress: () {
                       drawerProvider.seletedMenu(1);
                     },
                     isSelected: drawerProvider.selectedPageIndex == 1,
                   ),
                   MyDrawerMenu(
-                    title: "Products",
-                    icon: IconsAssets.products,
+                    title: "Category",
+                    icon: IconsAssets.category,
                     onPress: () {
                       drawerProvider.seletedMenu(2);
                     },
                     isSelected: drawerProvider.selectedPageIndex == 2,
+                  ),
+                  MyDrawerMenu(
+                    title: "Coupon",
+                    icon: IconsAssets.coupon,
+                    onPress: () {
+                      drawerProvider.seletedMenu(3);
+                    },
+                    isSelected: drawerProvider.selectedPageIndex == 3,
+                  ),
+                  MyDrawerMenu(
+                    title: "Orders",
+                    icon: IconsAssets.order,
+                    onPress: () {
+                      drawerProvider.seletedMenu(4);
+                    },
+                    isSelected: drawerProvider.selectedPageIndex == 4,
+                  ),
+                  MyDrawerMenu(
+                    title: "Settings",
+                    icon: IconsAssets.setting,
+                    onPress: () {
+                      drawerProvider.seletedMenu(5);
+                    },
+                    isSelected: drawerProvider.selectedPageIndex == 5,
                   ),
                 ],
               );
